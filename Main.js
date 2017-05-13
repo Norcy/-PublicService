6b58cf70cb44d1eba49f41461de5a7a31e8706e6
ChenYing <Norcy@users.noreply.github.com>
2017-05-14 05:20:20 +0800

    Update Main.js

--- Main.js
+++ Main.js
@@ -167,12 +167,30 @@ var hasUnlock = true;
   function preloadSth() 
   {
     console.log("preload");
+    var myVideo = document.getElementById("myVideo");
+    myVideo.muted = true;
+    myVideo.play();
 
-    var audio4 = document.getElementById("unlock");
+    var audio4 = document.getElementById('unlock');
     audio4.src = "audio/unlock.mp3";
     audio4.muted = true;
     audio4.currentTime = 0;
+    audio4.play();
     // audioAutoPlay(audio4);
+
+    var audio2 = document.getElementById('messageAudio2');
+    audio2.src = "audio/msg2s.mp3";
+    audio2.muted = true;
+    audio2.currentTime = 0;
+    audioAutoPlay(audio2);
+    // audio2.play();
+
+    var audio3 = document.getElementById('messageAudio3');
+    audio3.src = "audio/msg3s.mp3";
+    audio3.muted = true;
+    audio3.currentTime = 0;
+    // audio3.play();
+    audioAutoPlay(audio3);
 
     for (var i = 1; i <= 5; i++)
     {
@@ -188,26 +206,6 @@ var hasUnlock = true;
         }
       }
     }
-
-    var audio2 = document.getElementById('messageAudio2');
-    audio2.src = "audio/msg2s.mp3";
-    audio2.muted = true;
-    audio2.currentTime = 0;
-    // audioAutoPlay(audio2);
-    // audio2.play();
-
-    var audio3 = document.getElementById('messageAudio3');
-    audio3.src = "audio/msg3s.mp3";
-    audio3.muted = true;
-    audio3.currentTime = 0;
-    // audio3.play();
-    // audioAutoPlay(audio3);
-
-    var array = [audio4, audio2, audio3];
-    audioAutoPlay(array);
-    var myVideo = document.getElementById("myVideo");
-    myVideo.muted = true;
-    myVideo.play();
     
   }
 
@@ -239,41 +237,41 @@ var hasUnlock = true;
 
 function playAudio(audio) 
 {
-	stopAll();
-	audio.currentTime = 0;
+  stopAll();
+  audio.currentTime = 0;
   audio.muted = false;
-  audio.play();	
+  audio.play(); 
 }
 
 function stopAll()
 {
-	var audios = document.getElementsByName("audio");
-	for (var i = audios.length - 1; i >= 0; i--) 
-	{
-		audios[i].pause();
-	}
+  var audios = document.getElementsByName("audio");
+  for (var i = audios.length - 1; i >= 0; i--) 
+  {
+    audios[i].pause();
+  }
 
-	var myVideo = document.getElementById("myVideo");
-	if (myVideo)
-	{
-		myVideo.pause();
-	}
+  var myVideo = document.getElementById("myVideo");
+  if (myVideo)
+  {
+    myVideo.pause();
+  }
 }
 
 function showMessage() 
 {
-	var messageTips = document.getElementById("messageTips");
-	messageTips.className="messageTips";
-	messageTips.style.height = "104px";
-	var msgImg1 = document.getElementById("messageImg1");
-	msgImg1.src = "resource/Message1.png";
-	msgImg1.style.height = "100%";
-	console.log(msgImg1);
+  var messageTips = document.getElementById("messageTips");
+  messageTips.className="messageTips";
+  messageTips.style.height = "104px";
+  var msgImg1 = document.getElementById("messageImg1");
+  msgImg1.src = "resource/Message1.png";
+  msgImg1.style.height = "100%";
+  console.log(msgImg1);
 
-	var audio1 = document.getElementById('messageAudio1');
-	playAudio(audio1);
+  var audio1 = document.getElementById('messageAudio1');
+  playAudio(audio1);
 
-	setTimeout(function(){
+  setTimeout(function(){
     showPrintPage();
     var audio3 = document.getElementById('messageAudio3');
     playAudio(audio3);
@@ -282,43 +280,43 @@ function showMessage()
 
 function hideMessage() 
 {
-	var messageTips = document.getElementById("messageTips");
-	messageTips.style.width = 0;
-	messageTips.style.height = 0;
+  var messageTips = document.getElementById("messageTips");
+  messageTips.style.width = 0;
+  messageTips.style.height = 0;
 }
 
 function showMessage2() 
 {
-	var messageTips2 = document.getElementById("messageTips2");
-	messageTips2.className="messageTips";
-	messageTips2.style.height = "104px";
-	var msgImg2 = document.getElementById("messageImg2");
-	msgImg2.src = "resource/Message2.png";
-	msgImg2.style.height = "100%";
+  var messageTips2 = document.getElementById("messageTips2");
+  messageTips2.className="messageTips";
+  messageTips2.style.height = "104px";
+  var msgImg2 = document.getElementById("messageImg2");
+  msgImg2.src = "resource/Message2.png";
+  msgImg2.style.height = "100%";
 
-	var audio2 = document.getElementById('messageAudio2');
-	playAudio(audio2);
+  var audio2 = document.getElementById('messageAudio2');
+  playAudio(audio2);
 }
 
 function hideMessage2() 
 {
-	var messageTips2 = document.getElementById("messageTips2");
-	messageTips2.style.width = 0;
-	messageTips2.style.height = 0;
+  var messageTips2 = document.getElementById("messageTips2");
+  messageTips2.style.width = 0;
+  messageTips2.style.height = 0;
 }
 
 var didPlayEnd = false;
 
 function playVideo() 
 {
-	stopAll();
-	var viewport = document.getElementById("viewport");
-	viewport.style.display = "none";
+  stopAll();
+  var viewport = document.getElementById("viewport");
+  viewport.style.display = "none";
 
-	var VideoPage = document.getElementById("VideoPage");
-	VideoPage.className="VideoPage";
+  var VideoPage = document.getElementById("VideoPage");
+  VideoPage.className="VideoPage";
 
-	var myVideo = document.getElementById("myVideo");
+  var myVideo = document.getElementById("myVideo");
   myVideo.muted = false;
   myVideo.play();
   console.log(myVideo.className);
@@ -336,28 +334,28 @@ function showEndPage()
   }
   stopAll();
   $("#VideoPage").children().hide();
-	// $("#VideoPage").hide();
-	var VideoPage = document.getElementById("VideoPage");
-	// console.log(VideoPage.className);
-	VideoPage.style.width = "0";
-	VideoPage.style.height = "0";
+  // $("#VideoPage").hide();
+  var VideoPage = document.getElementById("VideoPage");
+  // console.log(VideoPage.className);
+  VideoPage.style.width = "0";
+  VideoPage.style.height = "0";
 
-	var endPage = document.getElementById("endPage");
-	endPage.className="endPage";
+  var endPage = document.getElementById("endPage");
+  endPage.className="endPage";
 }
 
 function showSharePage()
 {
-	console.log("showSharePage");
+  console.log("showSharePage");
 }
 
 
 function showPrintPage()
 {
-	hideMessage();
-	var printPage = document.getElementById("printPage");
-	printPage.className="printPage";
-	var msg="哥们，听别人说速度与激情8今天上映<br>";
+  hideMessage();
+  var printPage = document.getElementById("printPage");
+  printPage.className="printPage";
+  var msg="哥们，听别人说速度与激情8今天上映<br>";
   var msg2 = "这部电影我已经等了2年了<br>";
   var msg3 = "可是自从去年那场意外之后<br>";
   var msg4 = "我就再也没看过电影了<br><br>";
@@ -370,10 +368,10 @@ var i=1;
 var isBrace = false;
 function write(msg)  
 {  
-	var id=document.getElementById("printWord");  
-	var len=msg.length;  
-	console.log(i, len);
-	var msg1=msg.substring(0,i);  
+  var id=document.getElementById("printWord");  
+  var len=msg.length;  
+  console.log(i, len);
+  var msg1=msg.substring(0,i);  
   if (msg1.slice(-1) == '<')
   {
    isBrace = true;
@@ -391,39 +389,39 @@ id.innerHTML=msg1;
 }  
 function beginPrint(msg)  
 {
-	var t=setInterval(function(){
-		write(msg);	
-		if(i>=msg.length)
-		{
-			console.log("hehe")
-			clearInterval(t);
-			setTimeout(function(){
-				var printPage = document.getElementById("printPage");
-				printPage.style.width = "0";
-				printPage.style.height = "0";
+  var t=setInterval(function(){
+    write(msg); 
+    if(i>=msg.length)
+    {
+      console.log("hehe")
+      clearInterval(t);
+      setTimeout(function(){
+        var printPage = document.getElementById("printPage");
+        printPage.style.width = "0";
+        printPage.style.height = "0";
         write("");
 
         setTimeout(function(){
          showMessage2();
        },1000);
       }, 2000);
-		}  
-		else  
-		{
-			i++;  
-		}
-	}, 300);  
+    }  
+    else  
+    {
+      i++;  
+    }
+  }, 300);  
 }  
 
 function hideBtn()
 {
-	var btn = document.getElementById("btn");
-	btn.style.width = 0;
-	btn.style.height = 0;
+  var btn = document.getElementById("btn");
+  btn.style.width = 0;
+  btn.style.height = 0;
 
-	var audio = document.getElementById("unlock");
-	playAudio(audio);
-	hasUnlock = false;
+  var audio = document.getElementById("unlock");
+  playAudio(audio);
+  hasUnlock = false;
 }
 
 
@@ -445,16 +443,9 @@ function autoPlayAudio1(myAudio)
 }
 
 
-function audioAutoPlay(audioArray){
-
-    for (var i = 0; i < audioArray.length; i++)
-    {
-      audioArray[i].play(); 
-    }
+function audioAutoPlay(audio){
+    audio.play();
     document.addEventListener("WeixinJSBridgeReady", function () {
-        for (var i = 0; i < audioArray.length; i++)
-        {
-          audioArray[i].play(); 
-        }
+            audio.play();
     }, false);
 }
